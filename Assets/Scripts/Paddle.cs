4c341551f6ca62b40bd5e0624d0a8afef2d79e21
avin77 <shobhit.7@gmail.com>
2021-06-11 13:39:32 +0530

    commit the game with 3D

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float ScreenWidth=16f;
+    [SerializeField] float minScreenSize = 1f;
+    [SerializeField] float maxScreenSize = 15f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = (Input.mousePosition.x) / Screen.width * ScreenWidth;
+        Vector2 paddlePos = new Vector2(transform.position.x,transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minScreenSize, maxScreenSize);
+        transform.position = paddlePos;
+    }
+}
